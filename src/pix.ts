64d5bc6413fc44c3051e199a5da2b2d6d146eddc
Alan Philip <alanphilipdev@gmail.com>
2022-02-12 18:51:46 +0100

    remove subfolder

---
+++ src/pix.ts
@@ -0,0 +1,28 @@
+import { BigInt } from "@graphprotocol/graph-ts";
+import { Requested } from "./entities/PIX/PIX";
+import {
+  Global,
+  PIXRequested,
+} from "./entities/schema";
+import { PIX as PIXContract } from "../src/entities/PIX/PIX";
+
+const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
+
+
+export function handlePIXRequested(event: Requested): void {
+  let entity = Global.load("pixRequested");
+  if (entity == null) {
+    entity = new Global("pixRequested");
+    entity.value = new BigInt(0);
+  }
+
+  let pixRequested = new PIXRequested(entity.value.toString());
+  pixRequested.requestedId = entity.value;
+  pixRequested.dropId = event.params.dropId;
+  pixRequested.playerId = event.params.playerId;
+  pixRequested.mode = event.params.mode;
+  pixRequested.save();
+
+  entity.value = entity.value.plus(BigInt.fromI32(1));
+  entity.save();
+}
